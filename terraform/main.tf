202e43946c731b5e8d67b6af680f02f6dee734e2
Igor Stepanov <sigorilla@yandex-team.ru>
2019-03-29 15:38:31 +0300

    terraform

---
+++ terraform/main.tf
@@ -0,0 +1,60 @@
+terraform {
+  # Версия terraform
+  required_version = "0.11.13"
+}
+
+provider "google" {
+  # Версия провайдера
+  version = "2.0.0"
+
+  # ID проекта
+  project = "sigorilla-otus-devops-infra"
+  region  = "europe-west-1"
+}
+
+resource "google_compute_instance" "app" {
+  name         = "reddit-app"
+  machine_type = "g1-small"
+  zone         = "europe-west1-b"
+  tags         = ["reddit-app"]
+
+  # определение загрузочного диска
+  boot_disk {
+    initialize_params {
+      image = "reddit-base"
+    }
+  }
+
+  # определение сетевого интерфейса
+  network_interface {
+    # сеть, к которой присоединить данный интерфейс
+    network = "default"
+
+    # использовать ephemeral IP для доступа из Интернет
+    access_config {}
+  }
+
+  metadata {
+    # путь до публичного ключа
+    ssh-keys = "sigorilla:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+  name = "allow-puma-default"
+
+  # Название сети, в которой действует правило
+  network = "default"
+
+  # Какой доступ разрешить
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+
+  # Каким адресам разрешаем доступ
+  source_ranges = ["0.0.0.0/0"]
+
+  # Правило применимо для инстансов с перечисленными тэгами
+  target_tags = ["reddit-app"]
+}
