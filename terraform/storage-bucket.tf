8f04c2ab9a2a944ce1ca34686c70d4ddba33e0e4
Igor Stepanov <sigorilla@yandex-team.ru>
2019-04-02 20:29:25 +0300

    fmt

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -1,13 +1,15 @@
 provider "google" {
-    version = "2.0.0"
-    project = "${var.project}"
-    region = "${var.region}"
+  version = "2.0.0"
+  project = "${var.project}"
+  region  = "${var.region}"
 }
+
 module "storage-bucket" {
-    source = "SweetOps/storage-bucket/google"
-    version = "0.1.1"
-    name = ["storage-bucket-sigorilla-test"]
+  source  = "SweetOps/storage-bucket/google"
+  version = "0.1.1"
+  name    = ["storage-bucket-sigorilla-test"]
 }
+
 output storage-bucket_url {
-    value = "${module.storage-bucket.url}"
+  value = "${module.storage-bucket.url}"
 }
